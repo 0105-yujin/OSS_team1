b61122ed58e46f3da57a10bbff89707d6218cc7b
홍유진 <h66078053@gmail.com>
2025-10-28 11:14:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,61 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int main(void)
 {
-  printf("Hello");
-  printf("HI HI");
-  printf("hello world");
+  int num1, num2, result = 0;
+  char = op;
+
+  printf("사칙연산 계산기\n");
+  printf("입력 예시 : 3 + 4 \n");
+
+  while(1)
+    {
+      printf("입력(종료는 q입력) : ");
+
+      char line[100];
+      if(fgets(line, sizeof(line), stdin) == NULL)
+      {
+        puts("입력 종료");
+        return 0;
+      }
+
+      line[strcspn(line, "\r\n")] == '\0';
+
+      if(line[0] == 'q' || line[0] == 'Q')
+      {
+        return 0;
+      }
+
+      int parsed = sscanf(line, " %d %c %d", &num1, &op, &num2);
+      if(parsed != 3)
+      {
+        printf("입력 형식 오류\n\n");
+        continue;
+      }
+
+      switch(op)
+        {
+          case '+':
+            result = plus(num1,num2);
+          case '-':
+            result = minus(num1,num2);
+          case '*':
+            result = times(num1,num2);
+          case '/':
+            if (num2 == 0)
+            {
+              printf("지원하지 않는 입력 값\n\n");
+              continue;
+            }
+            result = divide(num1,num2);
+            break;
+          default:
+            printf("지원하지 않는 연산자\n",op);
+            printf("\n");
+            continue;
+        }
+      printf("결과 : %d %c %d = %d\n\n", num1, op, num2, result);
+    }
   return 0;
 }
