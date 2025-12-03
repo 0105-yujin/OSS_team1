1a2e4078b9852a78ccd22f6988a50c9fefcf68dc
이동호 <dlehdgh6550@naver.com>
2025-12-03 16:48:17 +0900

    [Update]beep

--- main.c
+++ main.c
@@ -468,11 +468,22 @@ int PlayRhythmGame() {
         long currentTime = GetTick() - startTime;
         int allFinished = 1;
 
-        if (_kbhit()) {
-            int key = _getch();
-            int line = -1;
-            if (key == 'd' || key == 'D') line = 0; else if (key == 'f' || key == 'F') line = 1;
-            else if (key == 'j' || key == 'J') line = 2; else if (key == 'k' || key == 'K') line = 3;
+            if (key == 'd' || key == 'D') { 
+                line = 0; 
+                Beep(262, 35); 
+            } 
+            else if (key == 'f' || key == 'F') { 
+                line = 1; 
+                Beep(294, 35); 
+            } 
+            else if (key == 'j' || key == 'J') { 
+                line = 2; 
+                Beep(330, 35);
+            } 
+            else if (key == 'k' || key == 'K') { 
+                line = 3; 
+                Beep(349, 35); 
+            } 
             else if (key == 'q' || key == 'Q') { gameRunning = 0; break; }
 
             if (line != -1) {
@@ -953,4 +964,5 @@ int PlayBossGame(int current_round) {
 
     return 1;
 }
+
 
