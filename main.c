3e65ff04bebe0ca2573b409e6bdb3319c6704cd4
이동호 <dlehdgh6550@naver.com>
2025-12-03 16:36:30 +0900

    [Add]Beep sound

--- main.c
+++ main.c
@@ -462,8 +462,24 @@ int PlayRhythmGame() {
         if (_kbhit()) {
             int key = _getch();
             int line = -1;
-            if (key == 'd' || key == 'D') line = 0; else if (key == 'f' || key == 'F') line = 1;
-            else if (key == 'j' || key == 'J') line = 2; else if (key == 'k' || key == 'K') line = 3;
+            
+
+            if (key == 'd' || key == 'D') { 
+                line = 0; 
+                Beep(262, 30); // 도 
+            } 
+            else if (key == 'f' || key == 'F') { 
+                line = 1; 
+                Beep(294, 30); // 레 
+            } 
+            else if (key == 'j' || key == 'J') { 
+                line = 2; 
+                Beep(330, 30); // 미 
+            } 
+            else if (key == 'k' || key == 'K') { 
+                line = 3; 
+                Beep(349, 30); // 파
+            } 
             else if (key == 'q' || key == 'Q') { gameRunning = 0; break; }
 
             if (line != -1) {
@@ -944,3 +960,4 @@ int PlayBossGame(int current_round) {
 
     return 1;
 }
+
